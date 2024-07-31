7fc25c6e1cf95cd5cdd3e726e1c062fb7bd10d08
Sourav Singh <souravsingh20161999@gmail.com>
2024-07-31 20:40:50 +0530

    Commit to Mango Repo

---
+++ Mango_Project/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("./model_mango.h5")
+CLASS_NAMES = ["Anthracnose", "Bacterial Canker", "Cutting Weevil", "Die Back", "Gall Midge", "Healthy", "Powdery Mildew", "Sooty Mould"]
+
+@app.get("/ping")
+async def ping():
+    return "Mango Disease Detection"
+
+def read_file_as_image(data) -> np.ndarray: 
+    img = Image.open(BytesIO(data))
+    resize_image = img.resize((180, 180), Image.LANCZOS)
+    res_image = np.array(resize_image)
+    return res_image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    prediction = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)*100
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = "localhost", port = 8010)
